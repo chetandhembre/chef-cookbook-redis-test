7435cd1b62aea395a85fd9f30d0981e1bcd5867c
chetandhembre <chetandhembre@gmail.com>
2014-07-08 14:54:46 +0530

    added nginx service

---
+++ ju-nginx/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: ju-nginx
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+cookbook_file "/etc/nginx/nginx.conf" do
+  source "nginx.conf"
+  mode "0644"
+end
+
+service 'nginx' do
+  action [ :enable, :restart ]
+end
